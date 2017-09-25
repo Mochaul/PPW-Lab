3ffbcde12cc3e9c7805d7aa9446ed1cddf3dca13
Mochaul <praditomo@gmail.com>
2017-09-25 10:41:24 +0700

    fix error

--- lab_4/tests.py
+++ lab_4/tests.py
@@ -34,8 +34,8 @@ class Lab4UnitTest(TestCase):
 
     def test_model_can_create_new_message(self):
         #Creating a new activity
-        self.assertEqual(str(new_activity),new_activity.message)
         new_activity = Message.objects.create(name=mhs_name,email='test@gmail.com',message='This is a test')
+        self.assertEqual(str(new_activity),new_activity.message)
 
         #Retrieving all available activity
         counting_all_available_message= Message.objects.all().count()
