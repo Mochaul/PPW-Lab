860331c396058206fac2098d288759b73df2c9d8
Mochaul <praditomo@gmail.com>
2017-11-05 21:06:40 +0700

    calculator

---
+++ lab_6/static/js/lab_6.js
@@ -0,0 +1,19 @@
+// Calculator
+var print = document.getElementById('print');
+var erase = false;
+
+var go = function(x) {
+  if (x === 'ac') {
+    /* implemetnasi clear all */
+  } else if (x === 'eval') {
+      print.value = Math.round(evil(print.value) * 10000) / 10000;
+      erase = true;
+  } else {
+    print.value += x;
+  }
+};
+
+function evil(fn) {
+  return new Function('return ' + fn)();
+}
+// END
