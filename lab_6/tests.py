19820e11908ecc43a7cce24cbbff2313cd4ed48a
Mochaul <praditomo@gmail.com>
2017-11-07 12:14:37 +0700

    fix test

--- lab_6/tests.py
+++ lab_6/tests.py
@@ -6,8 +6,3 @@ class Lab6Test(TestCase):
     def test_lab_6_url_is_exist(self):
         response = Client().get('/lab-6/')
         self.assertEqual(response.status_code, 200)
-
-    def test_root_url_now_is_using_index_page_from_lab_6(self):
-        response = Client().get('/')
-        self.assertEqual(response.status_code, 301)
-        self.assertRedirects(response, '/lab-6/', 301, 200)
