cfa5f3f1847235a53e2acf700a3de962c8eef6f5
Mochaul <praditomo@gmail.com>
2017-11-14 02:04:01 +0700

    finishing before

--- lab_7/models.py
+++ lab_7/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+# Create your models here.
 class Friend(models.Model):
-    friend_name = models.CharField(max_length=400)
-    npm = models.CharField(max_length=250)
-    added_at = models.DateField(auto_now_add=True)
+        friend_name = models.CharField(max_length=400)
+        npm = models.CharField(max_length=250)
+        alamat = models.CharField(max_length=400, default="-")
+        ttl = models.CharField(max_length=400, default="-")
+        prodi = models.CharField(max_length=100, default="-")
+        added_at = models.DateField(auto_now_add=True)
