1ec2ca2d8c565e38e9b51dcd3a0607f440b1b00e
Mochaul <praditomo@gmail.com>
2017-11-14 16:51:07 +0700

    edit tests

--- lab_7/api_csui_helper/csui_helper.py
+++ lab_7/api_csui_helper/csui_helper.py
@@ -13,13 +13,13 @@ class CSUIhelper:
             self.username = env("SSO_USERNAME")
             self.password = env("SSO_PASSWORD")
             self.client_id = 'X3zNkFmepkdA47ASNMDZRX3Z9gqSU1Lwywu5WepG'
-            self.access_token = self.get_access_token()
+            self.access_token = self.get_access_token(self.username, self.password)
 
-        def get_access_token(self):
+        def get_access_token(self, username, password):
             try:
                 url = "https://akun.cs.ui.ac.id/oauth/token/"
 
-                payload = "username=" + self.username + "&password=" + self.password + "&grant_type=password"
+                payload = "username=" + username + "&password=" + password + "&grant_type=password"
                 headers = {
                     'authorization': "Basic WDN6TmtGbWVwa2RBNDdBU05NRFpSWDNaOWdxU1UxTHd5d3U1V2VwRzpCRVFXQW43RDl6a2k3NEZ0bkNpWVhIRk50Ymg3eXlNWmFuNnlvMU1uaUdSVWNGWnhkQnBobUU5TUxuVHZiTTEzM1dsUnBwTHJoTXBkYktqTjBxcU9OaHlTNGl2Z0doczB0OVhlQ3M0Ym1JeUJLMldwbnZYTXE4VU5yTEFEMDNZeA==",
                     'cache-control': "no-cache",
@@ -30,25 +30,32 @@ class CSUIhelper:
 
                 return response.json()["access_token"]
             except Exception:
-                raise Exception("username atau password sso salah, input : [{}, {}] {}".format(self.username, self.password, os.environ.items()))
+                raise Exception("username atau password sso salah, input : [{}, {}] {}".format(username, password, os.environ.items()))
 
         def get_client_id(self):
             return self.client_id
 
         def get_auth_param_dict(self):
             dict = {}
-            access_token = self.get_access_token()
+            acces_token = self.get_access_token(self.username, self.password)
             client_id = self.get_client_id()
-            dict['access_token'] = access_token
+            dict['access_token'] = acces_token
             dict['client_id'] = client_id
 
             return dict
 
         def get_mahasiswa_list(self):
-            response = requests.get(API_MAHASISWA_LIST_URL,
-                                    params={"access_token": self.access_token, "client_id": self.client_id})
+            response = self.get_data_api(66)
             mahasiswa_list = response.json()["results"]
+            for i in range(67, 69):
+                response = self.get_data_api(i)
+                mahasiswa_list += response.json()["results"]
             return mahasiswa_list
+
+        def get_data_api(self, page):
+            response = requests.get(API_MAHASISWA_LIST_URL,
+                                    params={"access_token": self.access_token, "client_id": self.client_id, "page": page})
+            return response
 
     instance = None
 
