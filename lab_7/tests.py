86b0b1d8e192bf5e5b7fd728cad88fb5557b41e9
Mochaul <praditomo@gmail.com>
2017-11-14 15:36:23 +0700

    fix tests

--- lab_7/tests.py
+++ lab_7/tests.py
@@ -26,57 +26,47 @@ class lab7UnitTest(TestCase):
 		self.assertEqual(response.status_code, 200)
 
 	def test_get_friend_list_data_url_is_exist(self):
-		response = Client().get('/lab-7/get-friend-list/')
-		self.assertEqual(response.status_code, 200)
+	    response = Client().get('/lab-7/get-friend-list/')
+	    self.assertEqual(response.status_code, 200)
 
 	def test_friend_description_url_is_exist(self):
-		friend = Friend.objects.create(friend_name="Pina Korata", npm="1606123456")
-		response = Client().post('/lab-7/friend-list/description/' + str(friend.id) + '/')
-		self.assertEqual(response.status_code, 200)
+	    friend = Friend.objects.create(friend_name="Pina Korata", npm="1606123456")
+	    response = Client().post('/lab-7/detail/' + str(friend.id) + '/')
+	    self.assertEqual(response.status_code, 200)
 
 	def test_auth_param_dict(self):
-		csui_helper = CSUIhelper()
-		auth_param = csui_helper.instance.get_auth_param_dict()
-		self.assertEqual(auth_param['client_id'], csui_helper.instance.get_auth_param_dict()['client_id'])
+	    csui_helper = CSUIhelper()
+	    auth_param = csui_helper.instance.get_auth_param_dict()
+	    self.assertEqual(auth_param['client_id'], csui_helper.instance.get_auth_param_dict()['client_id'])
 
 	def test_add_friend(self):
-		response_post = Client().post(
-			'/lab-7/add-friend/',
-			{'name':"imran", 'npm':"1606", 'alamat':" ", 'ttl':" ", 'prodi':" "}
-		)
-		self.assertEqual(response_post.status_code, 200)
+	    response_post = Client().post(
+	        '/lab-7/add-friend/',
+	        {'name': "maap", 'npm': "1606", 'alamat': " ", 'ttl': " ", 'prodi': " "}
+	    )
+	    self.assertEqual(response_post.status_code, 200)
 
-	def test_invalid_sso_raise_exception(self):
-		username = "maap"
-		password = "maap"
-		csui_helper = CSUIhelper()
-		with self.assertRaises(Exception) as context:
-			csui_helper.instance.get_access_token(username, password)
-		self.assertIn("maap", str(context.exception))
+	def test_invalid_sso(self):
+	    csui_helper = CSUIhelper()
+	    csui_helper.instance.username = "maap"
+	    csui_helper.instance.password = "maap"
+	    with self.assertRaises(Exception) as context:
+	        csui_helper.instance.get_access_token()
+	    self.assertIn("maap", str(context.exception))
 
 	def test_validate_npm(self):
-		response = self.client.post('/lab-7/validate-npm/')
-		html_response = response.content.decode('utf8')
-		self.assertEqual(response.status_code, 200)
-		self.assertJSONEqual(html_response, {'is_taken':False})
+	    response = self.client.post('/lab-7/validate-npm/')
+	    html_response = response.content.decode('utf8')
+	    self.assertEqual(response.status_code, 200)
+	    self.assertJSONEqual(html_response, {'is_taken': False})
 
 	def test_delete_friend(self):
-		friend = Friend.objects.create(friend_name="Pina Korata", npm="1606123456")
-		response = Client().post('/lab-7/friend-list/delete-friend/' + str(friend.id) + '/')
-		self.assertEqual(response.status_code, 302)
-		self.assertNotIn(friend, Friend.objects.all())
+	    friend = Friend.objects.create(friend_name="Pina Korata", npm="1606123456")
+	    response = Client().post('/lab-7/delete/' + str(friend.id) + '/')
+	    self.assertEqual(response.status_code, 302)
+	    self.assertNotIn(friend, Friend.objects.all())
 
-	def test_invalid_page_pagination_number(self):
-		data = ["asik", "hehe", "seru", "asik", "hehe", "seru", "asik", "hehe", "seru", "asik",
-				"hehe", "seru", "asik", "hehe", "seru", "asik", "hehe", "seru", "asik", "hehe",
-				"seru", "asik", "hehe", "seru", "asik", "hehe", "seru", "asik", "hehe", "seru"]
-		test1 = paginate_page("...", data)
-		test2 = paginate_page(-1, data)
-		with patch.object(Manager, 'get_or_create') as a:
-			a.side_effect = PageNotAnInteger("page number is not an integer")
-			res = paginate_page("...", data)
-			self.assertEqual(res['page_range'], test1['page_range'])
-		with patch.object(Manager, 'get_or_create') as a:
-			a.side_effect = EmptyPage("page number is less than 1")
-			res = paginate_page(-1, data)
-			self.assertEqual(res['page_range'], test2['page_range'])
+	def test_can_get_mahasiswa_list(self):
+	    csui_helper = CSUIhelper()
+	    mhs_list = csui_helper.instance.get_mahasiswa_list()
+	    self.assertIsNotNone(mhs_list)
