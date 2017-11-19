d7cb56c581ca95d862cfbb026eae9862879dd691
Mochamad Aulia <praditomo@gmail.com>
2017-11-19 20:41:55 +0700

    testing lab

--- lab_8/views.py
+++ lab_8/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 
 # Create your views here.
+response = {}
+def index(request):
+	response['author'] = "Imran Amrulloh"
+	html = 'lab_8/lab_8.html'
+	return render(request, html, response)
