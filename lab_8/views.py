b1786ad8c2867cd0725032bd9af1245332a7823d
Mochaul <praditomo@gmail.com>
2017-11-21 14:06:24 +0700

    remove header

--- lab_8/views.py
+++ lab_8/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 # Create your views here.
 response = {}
 def index(request):
-	response['author'] = "Imran Amrulloh"
+	response['author'] = "Mochaul"
 	html = 'lab_8/lab_8.html'
 	return render(request, html, response)
