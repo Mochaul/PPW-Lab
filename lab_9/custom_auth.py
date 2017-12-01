8326bd32f3e432ac16ac234250c9d6c85cc961fe
Mochamad Aulia <praditomo@gmail.com>
2017-12-01 19:41:20 +0700

    fix auth

--- lab_9/custom_auth.py
+++ lab_9/custom_auth.py
@@ -12,21 +12,21 @@ def auth_login(request):
         username = request.POST['username']
         password = request.POST['password']
 
-        #call csui_helper
+    #call csui_helper
+    try:
         access_token = get_access_token(username, password)
-        if access_token is not None:
-            ver_user = verify_user(access_token)
-            kode_identitas = ver_user['identity_number']
-            role = ver_user['role']
+        ver_user = verify_user(access_token)
+        kode_identitas = ver_user['identity_number']
+        role = ver_user['role']
 
-            # set session
-            request.session['user_login'] = username
-            request.session['access_token'] = access_token
-            request.session['kode_identitas'] = kode_identitas
-            request.session['role'] = role
-            messages.success(request, "Anda berhasil login")
-        else:
-            messages.error(request, "Username atau password salah")
+        # set session
+        request.session['user_login'] = username
+        request.session['access_token'] = access_token
+        request.session['kode_identitas'] = kode_identitas
+        request.session['role'] = role
+        messages.success(request, "Anda berhasil login")
+    except Exception as e:
+        messages.error(request, "Username atau password salah")
     return HttpResponseRedirect(reverse('lab-9:index'))
 
 def auth_logout(request):
